8cea5c82bbbf98a97f958bf10b3acfd743e2bba7
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-08-01 20:15:12 -0400

    added event listener for respondWith

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -44,4 +44,17 @@ self.addEventListener("fetch", function(event){
      return;  
         
     }
+
+    event.respondWith(
+        fetch(event.request).catch(function(){
+            return caches.match(event.request).then(function(response){
+                if (response) {
+                    return response
+                } else if (event.request.headers.get("accept").includes("text/html"))
+                return caches.match("/")
+            })
+        })
+    )
+
+
 })
